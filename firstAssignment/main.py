d067a3cd6f86c2af4ae66e28727e539e00a61b9b
Naxus28 <gabrielferraz@Gabriel-Ferrazs-MacBook-Pro.local>
2014-05-06 12:18:29 -0400

    message in

--- firstAssignment/main.py
+++ firstAssignment/main.py
@@ -2,7 +2,7 @@ import math
 
 #input fields down below
 first_noun = raw_input('Please enter a noun:')
-second_noun = raw_input('Please enter another noun:')
+second_noun = raw_input('Please enter a verb:')
 third_noun = raw_input('Please enter a yet another noun:')
 fourth_noun = raw_input('Please enter a noun--again:')
 first_integer = raw_input('Please enter an integer:')
@@ -35,18 +35,24 @@ if third_integer and second_integer and first_integer:
 else:
     third_integer = 10
 
-#array and dictionary
 
+#array and dictionary
 array_noun = [first_noun,second_noun,third_noun,fourth_noun]
 
 integer = {'integer_one': first_integer, 'integer_two':second_integer,'integer_three':third_integer}
 
+new_integer = integer['integer_one']
 
+#for loop
+for i in range(0,2):
+    array_noun = [array_noun[3],array_noun[0], array_noun[1],array_noun[2]]
+    print array_noun
 
+#functions
 def integer_calc(x,y,z):
     return x+y-z
 
-function_one_result = integer_calc(first_integer,second_integer,third_integer)
+function_one_result = integer_calc(integer['integer_one'],integer['integer_two'],integer['integer_three'])
 print function_one_result
 
 
@@ -55,3 +61,15 @@ def integer_calc_two(x,y,z):
 
 function_two_result = integer_calc_two(first_integer,second_integer,third_integer)
 print function_two_result
+
+message = '''
+This is my {array_noun[3]} madlib experience!
+I did some {array_noun[1]} on google to learn what a madlib is.
+I looked up only {function_one_result} websites and got an {array_noun[2]} of what it is.
+It took me about {new_integer} minutes to write this text.
+Guess how long it took me to write the code... certainly not {fourth_integer} minutes. Maybe some {function_one_result} minutes.
+Crazy, huh?
+I hope you liked that your answers were all scrambled up and displayed in a different order than you entered them.
+Don't we love coding? ;)
+'''
+
