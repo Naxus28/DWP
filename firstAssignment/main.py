b9b47e9031f5080339adfa34d10e9d18a98e58a0
Naxus28 <gabrielferraz@Gabriel-Ferrazs-MacBook-Pro.local>
2014-05-05 23:36:55 -0400

    if else statements and mathematical operators in

--- firstAssignment/main.py
+++ firstAssignment/main.py
@@ -1,4 +1,4 @@
-
+import math
 
 #input fields down below
 first_noun = raw_input('Please enter a noun:')
@@ -9,4 +9,29 @@ first_integer = raw_input('Please enter an integer:')
 second_integer = raw_input('Please enter an integer:')
 third_integer = raw_input('Please enter another integer:')
 fourth_integer = raw_input('Please enter the last integer:')
+
+
+#if statements (last one has two logical operators),
+#  mathematical operators, assignment operators,
+if first_integer:
+    first_integer = (int(first_integer) + 21)
+    first_integer -= 3
+else:
+    first_integer = 1
+
+
+if second_integer:
+    second_integer = (int(second_integer) - 10)
+    second_integer += 11
+else:
+    second_integer = 4
+
+
+if third_integer and second_integer and first_integer:
+    third_integer = (int(third_integer) * 2)
+    third_integer /= 2
+    if third_integer % 2 != 0:
+        math.floor(third_integer)
+else:
+    third_integer = 10
 
