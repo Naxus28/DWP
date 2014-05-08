5919e23d3278f6bba5c60710bea84e14a44a0f78
Naxus28 <gabrielferraz@Gabriel-Ferrazs-MacBook-Pro.local>
2014-05-07 22:32:05 -0400

    second commit

--- ferraz_gabriel_Quiz1/main.py
+++ ferraz_gabriel_Quiz1/main.py
@@ -2,3 +2,9 @@
 def calc_area(w,h):
     if w == h:
         print "The width and height are the same. This is a square of: " + str(w * h) + " square feet"
+    else:
+        print "This is a rectangle of: " + str(w * h) + " square feet"
+        return w * h
+
+calc_area(20,20)
+calc_area(10,4)
