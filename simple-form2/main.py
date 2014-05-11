85160206306bc7cea8c490a8da527f9549383608
Naxus28 <gabrielferraz@Gabriel-Ferrazs-MacBook-Pro.local>
2014-05-11 10:45:05 -0400

    commit6

--- simple-form2/main.py
+++ simple-form2/main.py
@@ -19,9 +19,37 @@ from page import MyClass
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        # self.response.write('Hello world!')
-        first_view = MyClass()
-        self.response.write(first_view.main())
+        if self.request.GET:
+            name1 = self.request.GET["firstName"]
+            name2 = self.request.GET["lastName"]
+            keyword = self.request.GET["keyWord"]
+            gender = self.request.GET["gender"]
+            reason = self.request.GET["select"]
+            picture = self.request.GET["picture"]
+
+            if picture:
+                picture = picture
+            else:
+                picture = "images/temp_man.jpg"
+
+            if "checkbox" in self.request.GET:
+                checkbox = True
+            else:
+                checkbox = False
+
+            view = MyClass(name1, name2, keyword, gender, reason, picture)
+            if not checkbox:
+                view = MyClass()
+                self.response.write(view.main())
+            elif name1 and name2 and keyword and gender and reason and checkbox and picture:
+                self.response.write(view.page2())
+                print checkbox
+            else:
+                view = MyClass()
+                self.response.write(view.main())
+        else:
+            view = MyClass()
+            self.response.write(view.main())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
--- simple-form2/page.py
+++ simple-form2/page.py
@@ -1,6 +1,6 @@
 class MyClass(object):
-    def __init__(self):
-        self.page_open='''
+    def __init__(self, name1=" ", name2=" ", key=" ", gender=" ", reason=" ", picture=" "):
+        self.page_open = '''
 <!DOCTYPE html>
 <html>
     <head>
@@ -15,6 +15,8 @@ class MyClass(object):
             <h1>The Soccer Network</h1>
         </header>
         '''
+        #=====first page content=============
+        #left div, right div (form), and background picture
         self.content = '''
         <div class="container" id="left_container">
             <h2>WHAT YOU GET</h2>
@@ -31,26 +33,94 @@ class MyClass(object):
             <form method="GET" action="">
                 <input class="name" type="text" name="firstName" placeholder="First Name">
                 <input class="name" type="text" name="lastName" placeholder="Last Name">
-                <input class="password" type="text" name="password" placeholder="Password">
+                <input class="password" type="password" name="keyWord" placeholder="Password">
+                <input class="picture" type="text" name="picture" placeholder="Picture URL i.e. http://www.my_picture.com">
                 <p id="radio_par">
-                    <input class="radio" type="radio" name="genre" id="female" value="Female" /> <label class="radio" for="female">Female</label>
-                    <input class="radio" type="radio" name="genre" id="male" value="Male" /> <label class="radio" for="female">Male</label>
+                    <input class="radio" type="radio" name="gender" id="female" value="Female" checked="checked"/> <label class="radio" for="female">Female</label>
+                    <input class="radio" type="radio" name="gender" id="male" value="Male" /> <label class="radio" for="female">Male</label>
                 </p>
                 <select  name="select">
                      <option>I'm here to...</option>
-                     <option value="learn">learn more about soccer</option>
-                     <option value="find">find people to play with</option>
-                     <option value="read">read the news about soccer</option>
-                     <option value="message">exchange messages with soccer fans</option>
+                     <option value="learn more about soccer">learn more about soccer</option>
+                     <option value="find people to play with">find people to play with</option>
+                     <option value="read news about soccer">read news about soccer</option>
+                     <option value="exchange messages with soccer fans">exchange messages with soccer fans</option>
+                     <option value="have fun!">have fun!</option>
                 </select>
-               <p id="check"> <input type="checkbox" name="checkbox" class="checkbox" id="checkB"><label for="checkbox" class="checkbox">
-               I agree with the <a>terms of use</z></label></p>
+               <p id="check"> <input type="checkbox" value="true" name="checkbox" checked="checked" class="checkbox" id="checkB">
+               <label for="checkbox" class="checkbox">
+               I agree with the <a href="#">terms of use</a></label></p>
                <input id="submit" type="submit" name="submit" value="Sign up"/>
             </form>
         </div>
         '''
+        #this div is used for the background picture; it allows me
+        #to change the opacity of the bg picture without changing the opacity
+        #of the text--which would happen if I placed the pic on the body
         self.background_div = '''
         <div id="background"></div>
+        '''
+
+        #========second page content===========
+        self.open_container2 = '''
+        <div id="profile">
+        '''
+        self.open_h2 = '''
+            <h2 id="nameParagraph">
+            Welcome to The Soccer Network,
+        '''
+        self.name_h2 = " " + name1 + " " + name2
+
+        self.close_h2 = '''
+            </h2>
+        '''
+        self.open_picture = '''
+
+            <img value = "image" src="
+        '''
+        self.picture = picture
+
+        self.close_picture = '''
+            " />
+        '''
+
+        self.paragraph1 = '''
+            <p>
+            Here is your account information:
+            </p>
+        '''
+        self.open_password_paragraph = '''
+         <p id="passwordParagraph">
+        '''
+        self.password_paragraph = "Password: " + key
+
+        self.close_password_paragraph = '''
+            </p>
+        '''
+
+        self.open_gender = '''
+            <p id="gender">
+        '''
+        self.gender = "Gender: " + gender
+
+        self.close_gender = '''
+            </p>
+        '''
+
+        self.open_reason = '''
+            <p id="gender">
+        '''
+        self.reason = "The reason you signed up:" + reason
+
+        self.close_reason = '''
+            </p>
+        '''
+
+        self.close_container2 = '''
+        </div>
+        '''
+        self.background_div2 = '''
+        <div id="background2"></div>
         '''
         self.close_page = '''
     </body>
@@ -59,3 +129,10 @@ class MyClass(object):
 '''
     def main(self):
         return self.page_open + self.header + self.content + self.background_div + self.close_page
+
+    def page2(self):
+        return self.page_open + self.header + self.open_container2 + self.open_h2 + self.name_h2 + \
+               self.close_h2 + self.open_picture + self.picture + self.close_picture + self.paragraph1+ \
+               self.open_password_paragraph + self.password_paragraph +\
+               self.close_password_paragraph + self.open_gender + self.gender + self.close_gender + self.open_reason + \
+               self.reason + self.close_reason + self.close_container2 + self.background_div2 + self.close_page
