a274dc512bc99f7ccb10aa5af87bc3dbd0267fa1
Naxus28 <gabrielferraz@Gabriel-Ferrazs-MacBook-Pro.local>
2014-05-19 23:54:50 -0400

    commit 3

--- second-quiz/players.py
+++ second-quiz/players.py
@@ -6,9 +6,11 @@ class Player(object):
         self._speed = 0
         self._dribble = ""
         self._catch = ""
-        self._team = ""
-        self._salary = ""
-        self._team = ""
+        self._team = "This player doesn't have this information"
+        self._salary = "This player doesn't Have this information"
+        self._team = "This player doesn't have this information"
+        self._titles = "This player doesn't have this information"
+        self._age = "This player doesn't have this information"
 
     def print_out(self):
         self.update()
@@ -17,4 +19,6 @@ class Player(object):
     def update(self):
         self.all = self.__title + "</br>" + "Player Name: " + self._name +"</br>Player Position: " + self._position +\
                    "</br>Player Speed: " + str(self._speed) + "km/h" + "</br>Player Dribble Level: " + self._dribble + \
-                   "</br>Player Catch Level: " + self._catch + "This is the salary:" + str(self._salary) + "</br> </br>"
+                   "</br>Player Catch Level: " + self._catch + "</br>This is the salary:" + str(self._salary) +\
+                   "</br>This is the team:" + self._team + "</br>This is the number of titles:" + str(self._titles) +\
+                   "</br>This is the age: " + str(self._age)+ "</br> </br>"
