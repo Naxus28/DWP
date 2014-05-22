6a4403fe4e09eb75ba691f6d6f271d3ebf6e30b0
Naxus28 <gabrielferraz@Gabriel-Ferrazs-MacBook-Pro.local>
2014-05-21 23:34:25 -0400

    commit9

--- practice-quiz/main.py
+++ practice-quiz/main.py
@@ -4,11 +4,13 @@ import webapp2
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         p = Counter()
-
+        i = 1
         if self.request.GET:
             count = self.request.GET['name']
             if count == "the_function":
-                p.number = 1
+
+                p.number = i
+                #i = i + 1
                 self.response.write(p.print_out())
             else:
                 self.response.write(p.print_out())
