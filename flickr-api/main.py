f056004481580d6774e745444e2b3380e842eff0
Naxus28 <gabrielferraz@Gabriel-Ferrazs-MacBook-Pro.local>
2014-05-23 23:31:07 -0400

    flickr_API:3

--- flickr-api/main.py
+++ flickr-api/main.py
@@ -11,45 +11,54 @@ class MainHandler(webapp2.RequestHandler):
         view = FormPage()
         view.form_header = ""
 
-        #if there are URL variables...
-        # if self.request.GET:
-        #     query = self.request.GET['query']
-        #     #go get the api info
-        #     url_search =  "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=922fd10874987e031bb23715c945c8de&tags="+query+"&text="+query+"&content_type="+query+"&format=json&nojsoncallback=1&auth_token=72157644817258645-6445e7bd8ad6d5c9&api_sig=092c8362a74e4bed99152093fe3b36b0"
-        #     req_one = urllib2.Request(url_search)
-        #     opener_one = urllib2.build_opener()
-        #     #this is going to get the information for us
-        #     data_one = opener_one.open(req_one)
-        #     #parse it
-        #     jsondoc_one = json.load(data_one)
-        # the_search_urls = []
-        # for photo in range(0, 40):
-        #     farm_one = jsondoc['photos']['photo'][photo]['farm']
-        #     server_one = jsondoc['photos']['photo'][photo]['server']
-        #     the_id_one = jsondoc['photos']['photo'][photo]['id']
-        #     secret_one = jsondoc['photos']['photo'][photo]['secret']
-        #     the_search_urls =
+        #if there is an input
+        if self.request.GET:
+            query = self.request.GET['query']
+            #get the api info
+            url_search =  "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=922fd10874987e031bb23715c945c8de&tags="+query+"&text="+query+"&content_type="+query+"&format=json&nojsoncallback=1&auth_token=72157644817258645-6445e7bd8ad6d5c9&api_sig=092c8362a74e4bed99152093fe3b36b0"
+            req_one = urllib2.Request(url_search)
+            opener_one = urllib2.build_opener()
+            #this is going to get the information for us
+            data_one = opener_one.open(req_one)
+            #parse it
+            jsondoc_one = json.load(data_one)
 
+        the_search_urls = []
+        for photo in range(0, 40):
+            farm_one = jsondoc['photos']['photo'][photo]['farm']
+            server_one = jsondoc['photos']['photo'][photo]['server']
+            the_id_one = jsondoc['photos']['photo'][photo]['id']
+            secret_one = jsondoc['photos']['photo'][photo]['secret']
+            the_search_urls = the_search_urls.append("http://farm"+str(farm_one)+".staticflickr.com/"+str(server_one)+"/"+str(the_id_one)+"_"+str(secret_one)+".jpg")
+
+        for the_url in range(0, 40):
+            view.page_content += "<div class='img-container'><a href='"+the_urls[the_url]+ "'><img src ='"+the_urls[the_url]+"'></a></div>"
 
         #get the api info
         url = "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=84c81a688848153a0fa4db04702b63fd&format=json&nojsoncallback=1"
         req = urllib2.Request(url)
         opener = urllib2.build_opener()
-        #this is going to get the information for us
-        data = opener.open(req)
-        #parse it
 
+        #this is going to get the information
+        data = opener.open(req)
+
+        #array to hold the pictures returned
         the_urls = []
 
+        #parse the data
         jsondoc = json.load(data)
+
+        #loop through the pictures and get the necessary info to "build" 40 pictures
         for photo in range(0, 40):
             farm = jsondoc['photos']['photo'][photo]['farm']
             server = jsondoc['photos']['photo'][photo]['server']
             the_id = jsondoc['photos']['photo'][photo]['id']
             secret = jsondoc['photos']['photo'][photo]['secret']
 
+            #append the pictures to the array
             the_urls.append("http://farm"+str(farm)+".staticflickr.com/"+str(server)+"/"+str(the_id)+"_"+str(secret)+".jpg")
 
+        #push the pictures to the view
         for the_url in range(0, 40):
             view.page_content += "<div class='img-container'><a href='"+the_urls[the_url]+ "'><img src ='"+the_urls[the_url]+"'></a></div>"
 
