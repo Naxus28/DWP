519132384208b4867dedb2fd180e85b24a7b7a58
Naxus28 <gabrielferraz@Gabriel-Ferrazs-MacBook-Pro.local>
2014-05-30 19:58:04 -0400

    Final:2
    Basic page html created

---
+++ final-exam/model.py
@@ -0,0 +1,23 @@
+class PageView(object):
+    def __init__(self):
+        self.__open = '''
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>DPW Final Exam | Top 10 Songs<title>
+            </head>
+            <body>
+        '''
+        self.content = "the content"
+        self.__close = '''
+            </body>
+        </html>
+        '''
+        self._all = self.__open + self.content + self.__close
+
+    def print_out(self):
+        self.update()
+        return self._all
+
+    def update(self):
+        self._all = self.__open + self.content + self.__close
